06f7a2b72881912b9a03a8acf0eb01e14bedbcdf
1ierro1ast <lierro00@gmail.com>
2022-10-11 19:22:53 +0300

    last commit

---
+++ Assets/Codebase/Core/Character/WorldOutHandler.cs
@@ -0,0 +1,23 @@
+﻿using Codebase.Infrastructure.Services;
+using Codebase.Infrastructure.Services.Spawn;
+using UnityEngine;
+
+namespace Codebase.Core.Character
+{
+    public class WorldOutHandler : MonoBehaviour
+    {
+        [SerializeField] private MoveController _moveController;
+        private ISpawnPointsStorage _spawnPointsStorage;
+
+        private void Awake()
+        {
+            _spawnPointsStorage = AllServices.Container.Single<ISpawnPointsStorage>();
+        }
+
+        private void Update()
+        {
+            if (transform.position.y < -200)
+                _moveController.MoveToSpawnPoint(_spawnPointsStorage.GetSpawnPoint().position);
+        }
+    }
+}
